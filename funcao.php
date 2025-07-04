ba7d88fcbac77d9a531d4b109d043625ecc5daa5
Lara Gorito Barbosa de Souza <laragoritob@gmail.com>
2025-07-04 17:07:35 -0300

    versão 1

--- funcao.php
+++ funcao.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +7,14 @@
 </head>
 <body>
     <?php
-        $name = "Stefanie Hatcher";
-        $length = strlen($name);
-        $cmp = strcmp($name, "Brian Le");
-        $index = strpos($name, "e");
-        $first = substr($name, 9, 5);
-        $name = strtoupper($name);
+        $name = "Stefanie Hatcher"; 
+        $length = strlen($name); // Num de caracteres //
+        $cmp = strcmp($name, "Brian Le"); // Comparação de strings em ordem alfabética //
+        $index = strpos($name, "e"); // Posição do caractere em uma string //
+        $first = substr($name, 9, 5); // Parte de uma string //
+        $name = strtoupper($name); // Tudo em maiúsculo
 
-        echo("$name<br>");
+        echo("$name<br>"); 
         echo("$length<br>");
         echo("$cmp<br>");
         echo("$index<br>");
---
+++ string.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $idade = 16;
+
+        print "Você tem " . $idade . " anos. <br>"; 
+        print "Você tem $idade anos. <br>"; // STRINGS DENTRO DE ASPAS SÃO INTERPRETADAS //
+        print 'Você tem $idade anos. <br>'; // STRINGS EM ASPAS SIMPLES NÃO SÃO INTERPRETADAS //
+        print "Hoje é seu $idadeº aniversário. <br>"; // NÃO FUNCIONA POR CAUSA DO º ESTAR JUNTO DA VARIÁVEL //
+        print "Hoje é seu {$idade}º aniversário. <br>"; // { } SERVE PARA SEPARAR A VARIÁVEL DO TEXTO //
+    ?>
+
+    <br><br>
+
+    <?php
+        $cidade = "Petrópolis";
+        $estado = "RJ";
+        $idade = 182;
+        $frase_capital = "A cidade de $cidade é serrana e faz parte do estado do $estado. <br>";
+        $frase_idade = "$cidade tem mais de $idade anos. <br>";
+        
+        echo "<h3>$frase_capital</h3>";
+        echo "<h4>$frase_idade</h4>";
+    ?>
+</body>
+</html>
